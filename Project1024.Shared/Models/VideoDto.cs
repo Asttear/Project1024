0d3eafd9b3ffeef03ef3dc9426129f7c73da924f
Asttear <asttear@sina.cn>
2023-11-01 00:58:02 +0800

    添加视频DTO

---
+++ Project1024.Shared/Models/VideoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Project1024.Shared.Models;
+
+public record VideoDto
+(
+    int Id,
+    int CategoryId,
+    string Title,
+    DateTimeOffset CreatedTime,
+    string CoverUrl,
+    string VideoUrl
+);
