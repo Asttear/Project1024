aab7eede11e66c76742879801f1261110198f1d0
Asttear <asttear@sina.cn>
2023-11-07 02:45:22 +0800

    视频切换相关优化

--- Project1024.Wasm/Components/Swiper/SwiperContainer.razor.js
+++ Project1024.Wasm/Components/Swiper/SwiperContainer.razor.js
@@ -1,3 +1,17 @@
-export function getSwiper(swiperContainer) {
-    return swiperContainer.swiper;
+let helper;
+
+export function init(el, dotnetHelper) {
+    helper = dotnetHelper;
+    el.addEventListener("swiperrealindexchange", onRealIndexChange);
+    return el.swiper;
+}
+
+export function destroy() {
+    el.removeEventListener("swiperrealindexchange", onRealIndexChange);
+}
+
+async function onRealIndexChange(e) {
+    if (true) {
+        await helper.invokeMethodAsync("OnIndexChanged", e.detail[0].realIndex);
+    }
 }
---
+++ Project1024.Wasm/Components/Swiper/SwiperJSHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace Project1024.Wasm.Components.Swiper;
+
+public class SwiperJSHelper
+{
+    private readonly SwiperContainer _swiper;
+
+    public SwiperJSHelper(SwiperContainer swiper)
+    {
+        _swiper = swiper;
+    }
+
+    [JSInvokable]
+    public async Task OnIndexChanged(int index)
+    {
+        await _swiper.IndexChanged.InvokeAsync(index);
+    }
+}
